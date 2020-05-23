7cc51a752f5c226d224fe6e2a17ee26b58bc7650
araldhafeeri <ar.aldhafeeri11@gmail.com>
2020-05-23 00:54:00 -0400

    5-23-2020 VP page 0-18

---
+++ ViolentPython/scan-files.py
@@ -0,0 +1,4 @@
+import sys
+if len(sys.argv)==2:
+     filename = sys.argv[1]
+     print("[+] Scanning Vulnerabilities From:{}".format(filename))
---
+++ ViolentPython/variable.py
@@ -0,0 +1,4 @@
+port = 21
+banner = "FreeFloat FTP server"
+print("[+] Checking  for {} on port {}".format(banner,port))
+
