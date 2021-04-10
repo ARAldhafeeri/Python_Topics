0e34beca4666fb3ce52aec57b175535f37909c87
araldhafeeri <ar.aldhafeeri11@gmail.com>
2021-04-10 11:13:28 +0300

    heap doc

--- leetcode/Counter_heap.py
+++ leetcode/Counter_heap.py
@@ -7,9 +7,13 @@ Example 1:
 Input: nums = [1,1,1,2,2,3], k = 2
 Output: [1,2]
 """
+import heapq
+from collections import Counter
+
 
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         if len(nums) == 1: return [nums[0]]
         hashtable = Counter(nums)
+        # heapq.nlargest(selectedCount, iterable, key=callable_funciton)
         return heapq.nlargest(k, hashtable.keys(), key=hashtable.get)
