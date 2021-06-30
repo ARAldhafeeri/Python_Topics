16498ff99b4ef638d7af7dd0708b88b223e2bf87
araldhafeeri <ar.aldhafeeri11@gmail.com>
2021-06-30 05:39:59 +0300

    solved yesterday hard problem

--- leetcode/Arrays_and_linked_lists/day3/lfu_cache.py
+++ leetcode/Arrays_and_linked_lists/day3/lfu_cache.py
@@ -61,3 +61,53 @@ class LFUCache:
 # obj = LFUCache(capacity)
 # param_1 = obj.get(key)
 # obj.put(key,value)
+
+
+class LFUCache:
+
+    def __init__(self, capacity):
+        """
+        :type capacity: int
+        """
+        self.cache = {}
+        self.count = collections.defaultdict(collections.OrderedDict)
+        self.min = 0
+        self.capacity = capacity
+
+    def get(self, key):
+        """
+        :type key: int
+        :rtype: int
+        """
+        if key not in self.cache:
+            return -1
+        value, count = self.cache[key]
+        del self.count[count][key]
+        if count == self.min and not self.count[count]:
+            self.min += 1
+        self.count[count+1][key] = 0
+        self.cache[key] = (value, count+1)
+        return value
+
+    def put(self, key, value):
+        """
+        :type key: int
+        :type value: int
+        :rtype: void
+        """
+        if self.capacity <= 0:
+            return
+        if key in self.cache:
+            old, count = self.cache[key]
+            self.cache[key] = (value, count)
+            self.get(key)
+        elif len(self.cache) == self.capacity:
+            old_key, v = self.count[self.min].popitem(last=False)
+            del self.cache[old_key]
+            self.min = 1
+            self.cache[key] = (value, 1)
+            self.count[1][key] = 0
+        else:
+            self.min = 1
+            self.cache[key] = (value, 1)
+            self.count[1][key] = 0
